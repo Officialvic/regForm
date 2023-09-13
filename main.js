61f50872b842f1a5481dd400c4b265aa583b17c8
OfficialVic <onaopeadeoye594@gmail.com>
2023-09-13 09:42:45 +0100

    localStorgae logic

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-// collect data for registered users
-// let regForm = document.forms['regForm'];
-// let data = new FormData(regForm);
-
-
-
-
-
-
 // To save registration data on localStorage
 let saveData = (e) => {
     e.preventDefault();
 
+    // collect data for registered users
     let firstName = document.getElementById('firstName').value;
     let lastName = document.getElementById('lastName').value;
     let email = document.getElementById('email').value;
@@ -22,12 +14,35 @@ let saveData = (e) => {
             email,
             password
         }
-        localStorage.setItem(users, (JSON.stringify(users)))
+        localStorage.setItem('users', (JSON.stringify(users)))
+}
 
-//     for (let [key, value] of data) {
-//     users.innerHTML = data.get(key);
-// }
-//     localStorage.setItem('users', (JSON.stringify(users)));
+// To Check for login.
+let login = (e) => {
+    e.preventDefault();
+    
+    let registeredUsers = JSON.parse((localStorage.getItem('users')))
+
+    let loginEmail = document.getElementById('loginEmail').value
+    let loginPassword = document.getElementById('loginPassword').value
+
+    let loginDetail = {
+        loginEmail,
+        loginPassword
+    }
+
+    loginDetail = JSON.stringify(loginDetail)
+    
+    let filteredDetail = JSON.parse(loginDetail);
+    console.log(filteredDetail) 
+    console.log(registeredUsers)
+    console.log(registeredUsers.email)
+
+    if (filteredDetail.loginEmail === registeredUsers.email && filteredDetail.loginPassword === registeredUsers.password) {
+        alert('Welcome onboard dear user');
+    } else {
+        alert("Incorrect Email or Password. Kindly signup if you don't an account yet")
+    }
 }
 
 
